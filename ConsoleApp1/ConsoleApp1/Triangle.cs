0e4364cf56a15b8db0681a9e33d4fdc721864085
Gianfra100 <gianfra2400@gmail.com>
2023-04-05 23:01:36 -0500

    Interfaz perimetro triangulo

--- ConsoleApp1/ConsoleApp1/Triangle.cs
+++ ConsoleApp1/ConsoleApp1/Triangle.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Triangle : Shape, Area
+    class Triangle : Shape, Area, Perimeter
     {
-        private float b;
-        private float h;
+        float b;
+        float h;
 
         public Triangle(float b, float h)
         {
             this.name = "Triángulo";
             this.b = b;
             this.h = h;
+        }
+
+        public float CalculatePerimeter()
+        {
+            return b * 3; 
         }
 
         public float CalculetArea()
