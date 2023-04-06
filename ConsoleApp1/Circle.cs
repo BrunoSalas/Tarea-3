272f9e0a647759c99424ee4a58234e1c44abae7a
Rodrigo Cordova Melendez <65469186+rodrigocormel@users.noreply.github.com>
2023-04-05 22:51:23 -0500

    interfaz perimetro circulo

---
+++ ConsoleApp1/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Circle : Shape, Area, Perimeter
+    {
+        private float radius;
+
+        public Circle(float radius)
+        {
+            this.name = "Círculo";
+            this.radius = radius;
+        }
+
+        public float CalculatePerimeter()
+        {
+            return (2 * 3.14f) * radius;
+        }
+
+        public float CalculetArea()
+        {
+            return radius * radius * 3.14f;
+        }
+    }
+}
